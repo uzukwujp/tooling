0840dd5cc29dc372607849fffb3a20c74e85023d
Uzukwu, John Paul <52359007+uzukwujp@users.noreply.github.com>
2022-04-21 14:15:46 +0100

    Update login.php
    changes login.php

--- html/login.php
+++ html/login.php
@@ -25,7 +25,7 @@
 			<button type="submit" class="btn" name="login_btn">Login</button>
 		</div>
 	<!--	<p>
-			Is not yet a member? <a href="register.php">Sign up</a>
+			 Not yet a member? <a href="register.php">Sign up</a>
 		</p> -->
 	</form>
 </body>
